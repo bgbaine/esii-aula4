b1d50cfd98e7725880b39e3f14dc1f4a155268e6
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-04-04 23:49:04 -0300

    feature/adiciona e implementa os métodos frear e subirMarcha

--- index.ts
+++ index.ts
@@ -23,7 +23,15 @@ while(true){
         case 1:
             acelerar(carro);
             break;
-    
+
+        case 2:
+            frear(carro);
+            break;
+
+        case 3:
+            subirMarcha(carro);
+            break;
+
         default:
             break;
     }
@@ -36,6 +44,29 @@ function acelerar(veiculo: Veiculo): void{
     veiculo.velocidade += veiculo.potencia*0.2;
     console.log(veiculo.velocidade);
 }}
+
+function frear(veiculo: Veiculo): void{
+    veiculo.velocidade = 0;
+    console.log(veiculo.velocidade);
+}
+
+function subirMarcha(veiculo: Veiculo): void{
+    if (veiculo.marchaAtual === veiculo.numeroMarchas) {
+        veiculo.marchaAtual = veiculo.numeroMarchas;
+        console.log('O veículo ja esta na última marcha: ' + veiculo.marchaAtual);
+        return;
+    }
+    if (veiculo.marchaAtual === 0) {
+        veiculo.marchaAtual = 1;
+        console.log(veiculo.marchaAtual);
+        return;
+    }
+    if (veiculo.marchaAtual < veiculo.numeroMarchas) {
+        veiculo.marchaAtual++;
+        console.log(veiculo.marchaAtual);
+        return;
+    }
+}
 
 function criaVeiculo(): Veiculo{
     const veiculo: Veiculo = new Veiculo();
